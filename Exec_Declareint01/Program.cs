9dfecfe5ff708e43c7659f140497cb038cccbf99
Tim <vunvun0213@gmail.com>
2022-10-17 01:09:08 +0800

    Bug

--- Exec_Declareint01/Program.cs
+++ Exec_Declareint01/Program.cs
@@ -10,6 +10,28 @@ namespace Exec_Declareint01
     {
         static void Main(string[] args)
         {
+            int number1 = 1 + ((2 + 2) * 3) - 1;
+            Console.WriteLine(number1);
+
+            int number2 = 120 / 7;
+            int number3 = 120 % 7;
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+
+            int number4 = 9 % 2;
+            Console.WriteLine(number4);
+            int number5 = 202 % 2;
+            Console.WriteLine(number5);
+
+            int number6 = 0;
+            int number7 = 1;
+            int exchange = number6;
+
+            number6 = number7;
+            number7 = exchange;
+
+            Console.WriteLine($"對調結果:number6={number6},number7={number7}");
+            
         }
     }
 }
